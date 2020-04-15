711fa2eecd6526af7d5e56f45db20d4a00f8b5fa
Victor Okpon <vikzy2rhyme@yahoo.com>
2020-04-15 01:11:27 +0100

    v2

--- estimator.js
+++ estimator.js
@@ -17,6 +17,19 @@ const dataInput = {
 let dataParsed;
 
 const covid19ImpactEstimator = (data) => {
+let dataParsed = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
   const convertData = () => {
     dataParsed = data;
     return dataParsed;
